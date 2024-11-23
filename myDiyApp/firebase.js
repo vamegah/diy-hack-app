4bc90df7e81f1eece85e2a3f19fd6bc481f40380
lavskillup <73978832+lavskillup@users.noreply.github.com>
2024-11-23 16:21:51 +1100

    Update firebase.js

--- myDiyApp/firebase.js
+++ myDiyApp/firebase.js
@@ -2,21 +2,24 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "@firebase/auth";
 import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
+// Uncomment the below line if you want to try the app with Firebase app storage
+// You must have upgraded your Firebae account to use this
+// import { getStorage } from 'firebase/storage';
 
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyByvHyTPvwSD5iI9gFMeKk89eCoL3P__Ss",
-  authDomain: "reactnative-push-notific-e871b.firebaseapp.com",
-  projectId: "reactnative-push-notific-e871b",
-  storageBucket: "reactnative-push-notific-e871b.appspot.com",
-  messagingSenderId: "98427882719",
-  appId: "1:98427882719:web:8a17a006cb531468eaed6a"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app);
+// Uncomment the below line if you are trying the app with Firebase app storage
+// export const storage = getStorage(app);
